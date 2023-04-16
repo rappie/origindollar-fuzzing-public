0d8f1abf34ee7c2ccc1326f293b2a78fdb21f657
Rappie <jasper@rappie.com>
2023-04-16 21:26:25 +0200

    Debugging stuff

--- src/echidna/EchidnaDebug.sol
+++ src/echidna/EchidnaDebug.sol
@@ -3,17 +3,11 @@
 import "./EchidnaHelper.sol";
 import "./Debugger.sol";
 
+import "../token/OUSD.sol";
+
 contract EchidnaDebug is EchidnaHelper {
-    bool debug = true;
-
-    // bool debug = false;
-
-    function debugOUSDBalance() public view returns (uint256) {
-        assert(ousd.balanceOf(ADDRESS_USER0) == 0);
-    }
-
-    function debugEchidna() public {
-        require(debug);
-        assert(false);
+    function debugOUSD() public view returns (uint256) {
+        // assert(ousd.balanceOf(ADDRESS_USER0) == 1000);
+        // assert(ousd.rebaseState(ADDRESS_USER0) != OUSD.RebaseOptions.OptIn);
     }
 }
