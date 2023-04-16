bcb0f9d4f953c58832ec7959c485e8a66ef2abc4
Rappie <jasper@rappie.com>
2023-04-16 20:56:34 +0200

    Setup

--- src/echidna/EchidnaSetup.sol
+++ src/echidna/EchidnaSetup.sol
@@ -3,6 +3,17 @@
 import "./IHevm.sol";
 import "./EchidnaConfig.sol";
 
+import "../token/OUSD.sol";
+
 contract EchidnaSetup is EchidnaConfig {
     IHevm hevm = IHevm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
+    OUSD ousd = new OUSD();
+
+    constructor() {
+        ousd.initialize("Origin Dollar", "OUSD", ADDRESS_VAULT);
+
+        hevm.prank(ADDRESS_VAULT);
+        ousd.mint(ADDRESS_USER0, 1000);
+
+    }
 }
