205aacf350692db049adca7311a03c09557b04aa
Rappie <jasper@rappie.com>
2023-04-17 15:06:11 +0200

    Refactor

--- src/echidna/EchidnaTest.sol
+++ src/echidna/EchidnaTest.sol
@@ -6,28 +6,6 @@ import "./EchidnaDebug.sol";
 import "./Debugger.sol";
 
 contract EchidnaTest is EchidnaSetup, EchidnaHelper, EchidnaDebug {
-    // An account should always be able to successfully transfer an amount within its balance.
-    function testTransferWithinBalanceDoesNotRevert(
-        uint8 fromAcc,
-        uint8 toAcc,
-        uint256 amount
-    ) public {
-        address from = getAccount(fromAcc);
-        address to = getAccount(toAcc);
-
-        require(amount > 0);
-        amount = amount % ousd.balanceOf(from);
-
-        Debugger.log("Total supply", ousd.totalSupply());
-
-        hevm.prank(from);
-        try ousd.transfer(to, amount) {
-            assert(true);
-        } catch {
-            assert(false);
-        }
-    }
-
     // The receiving account's balance after a transfer must increase by at least the amount transferred
     // The sending account's balance after a transfer must decrease by no more than amount transferred.
     function testTransferBalance(
@@ -56,5 +34,27 @@ contract EchidnaTest is EchidnaSetup, EchidnaHelper, EchidnaDebug {
 
         assert(fromBalAfter >= fromBalBefore - amount);
         assert(toBalAfter >= toBalBefore + amount);
+    }
+
+    // An account should always be able to successfully transfer an amount within its balance.
+    function testTransferWithinBalanceDoesNotRevert(
+        uint8 fromAcc,
+        uint8 toAcc,
+        uint256 amount
+    ) public {
+        address from = getAccount(fromAcc);
+        address to = getAccount(toAcc);
+
+        require(amount > 0);
+        amount = amount % ousd.balanceOf(from);
+
+        Debugger.log("Total supply", ousd.totalSupply());
+
+        hevm.prank(from);
+        try ousd.transfer(to, amount) {
+            assert(true);
+        } catch {
+            assert(false);
+        }
     }
 }
