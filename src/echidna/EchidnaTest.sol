f7cbc8e5e199b0dad14c891b7fd49da28bbc60d7
Rappie <jasper@rappie.com>
2023-04-17 13:30:58 +0200

    testTransferWithinBalanceDoesNotRevert

--- src/echidna/EchidnaTest.sol
+++ src/echidna/EchidnaTest.sol
@@ -6,6 +6,26 @@ import "./EchidnaDebug.sol";
 import "./Debugger.sol";
 
 contract EchidnaTest is EchidnaSetup, EchidnaHelper, EchidnaDebug {
+    // An account should always be able to successfully transfer an amount within its balance.
+    function testTransferWithinBalanceDoesNotRevert(
+        uint8 fromAcc,
+        uint8 toAcc,
+        uint256 amount
+    ) public {
+        address from = getAccount(fromAcc);
+        address to = getAccount(toAcc);
+
+        require(amount > 0);
+        amount = amount % ousd.balanceOf(from);
+
+        hevm.prank(from);
+        try ousd.transfer(to, amount) {
+            assert(true);
+        } catch {
+            assert(false);
+        }
+    }
+
     // The receiving account's balance after a transfer must increase by at least the amount transferred
     // The sending account's balance after a transfer must decrease by no more than amount transferred.
     // function testTransferBalance(
