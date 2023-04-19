2f44e031c0cfdfd6e78634c4a73befafe78ed8f1
Rappie <jasper@rappie.com>
2023-04-19 12:17:56 +0200

    Refactor

--- src/echidna/EchidnaDebug.sol
+++ src/echidna/EchidnaDebug.sol
@@ -7,7 +7,7 @@ import "./Debugger.sol";
 import "../token/OUSD.sol";
 
 contract EchidnaDebug is EchidnaHelper {
-    function debugOUSD() public view returns (uint256) {
+    function debugOUSD() public view {
         // assert(ousd.balanceOf(ADDRESS_USER0) == 1000);
         // assert(ousd.rebaseState(ADDRESS_USER0) != OUSD.RebaseOptions.OptIn);
         assert(Address.isContract(ADDRESS_CONTRACT0));
