843960ffb6ad50ba15f1024b2df4b84607706857
Rappie <jasper@rappie.com>
2023-07-30 10:56:34 +0200

    NatSpec

--- src/echidna/Echidna.sol
+++ src/echidna/Echidna.sol
@@ -3,4 +3,8 @@ pragma solidity ^0.8.0;
 
 import "./EchidnaTestMintBurn.sol";
 
+/** @title Echidna test contract for OUSD
+  * @notice Target contract to be tested, containing all mixins
+  * @author Rappie
+  */
 contract Echidna is EchidnaTestMintBurn {}
