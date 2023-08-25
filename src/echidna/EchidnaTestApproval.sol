7d93121ab303803de7676562dc4f8230af01ffaf
Rappie <jasper@rappie.com>
2023-08-25 12:15:48 +0200

    EchidnaTestApproval mixin

---
+++ src/echidna/EchidnaTestApproval.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./EchidnaTestMintBurn.sol";
+import "./Debugger.sol";
+
+/**
+ * @title Mixin for testing approval related functions
+ * @author Rappie
+ */
+contract EchidnaTestApproval is EchidnaTestMintBurn {
+}
