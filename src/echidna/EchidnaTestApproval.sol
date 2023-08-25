25f4ae5c7a20ff7b9ebdfe388b5c8eec62603159
Rappie <jasper@rappie.com>
2023-08-25 12:23:15 +0200

    testTransferFromShouldNotRevert

--- src/echidna/EchidnaTestApproval.sol
+++ src/echidna/EchidnaTestApproval.sol
@@ -9,4 +9,31 @@ import "./Debugger.sol";
  * @author Rappie
  */
 contract EchidnaTestApproval is EchidnaTestMintBurn {
+    /**
+     * @notice Performing `transferFrom` with an amount inside the allowance should not revert
+     * @param authorizedAcc The account that is authorized to transfer
+     * @param fromAcc The account that is transferring
+     * @param toAcc The account that is receiving
+     * @param amount The amount to transfer
+     */
+    function testTransferFromShouldNotRevert(
+        uint8 authorizedAcc,
+        uint8 fromAcc,
+        uint8 toAcc,
+        uint256 amount
+    ) public {
+        address authorized = getAccount(authorizedAcc);
+        address from = getAccount(fromAcc);
+        address to = getAccount(toAcc);
+
+        require(amount <= ousd.balanceOf(from));
+        require(amount <= ousd.allowance(from, authorized));
+
+        hevm.prank(authorized);
+        try ousd.transferFrom(from, to, amount) {
+            // pass
+        } catch {
+            assert(false);
+        }
+    }
 }
